808e140e3a2e52e274f57b3b74afb0e153d77010
Rémy Coqueugniot <remy.coqueugniot@skyguide.ch>
2017-05-15 14:54:16 +0200

    Introducing Cube rotation

--- src/main/java/org/xflash/engine/Window.java
+++ src/main/java/org/xflash/engine/Window.java
@@ -94,6 +94,8 @@ public class Window {
 
         // Set the clear color
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        glEnable(GL_DEPTH_TEST);
     }
 
     public void setClearColor(float r, float g, float b, float alpha) {
--- src/main/java/org/xflash/gm/flw/flw02/DummyGame.java
+++ src/main/java/org/xflash/gm/flw/flw02/DummyGame.java
@@ -31,25 +31,54 @@ public class DummyGame implements IGameLogic {
         renderer = new Renderer();
     }
 
+    //                        +41788862435
+
+
     @Override
     public void init(Window window) throws Exception {
         renderer.init(window);
         GameItem gameItem = new GameItem(
                 new Mesh(new float[]{
-                        -0.5f, 0.5f, -1.05f, //0
-                        -0.5f, -0.5f, -1.05f, //1
-                        0.5f, -0.5f, -1.05f, //2
-                        0.5f, 0.5f, -1.05f, //3
+                        // VO
+                        -0.5f, 0.5f, 0.5f,
+                        // V1
+                        -0.5f, -0.5f, 0.5f,
+                        // V2
+                        0.5f, -0.5f, 0.5f,
+                        // V3
+                        0.5f, 0.5f, 0.5f,
+                        // V4
+                        -0.5f, 0.5f, -0.5f,
+                        // V5
+                        0.5f, 0.5f, -0.5f,
+                        // V6
+                        -0.5f, -0.5f, -0.5f,
+                        // V7
+                        0.5f, -0.5f, -0.5f,
                 },
                         new float[]{
                                 0.5f, 0.0f, 0.0f,
                                 0.0f, 0.5f, 0.0f,
                                 0.0f, 0.0f, 0.5f,
                                 0.0f, 0.5f, 0.5f,
+                                0.5f, 0.0f, 0.0f,
+                                0.0f, 0.5f, 0.0f,
+                                0.0f, 0.0f, 0.5f,
+                                0.0f, 0.5f, 0.5f,
                         },
                         new int[]{
-                                0, 1, 3,
-                                3, 1, 2,
+                                // Front face
+                                0, 1, 3, 3, 1, 2,
+                                // Top Face
+                                4, 0, 3, 5, 4, 3,
+                                // Right face
+                                3, 2, 7, 5, 3, 7,
+                                // Left face
+                                6, 1, 0, 6, 0, 4,
+                                // Bottom face
+                                2, 1, 6, 2, 6, 7,
+                                // Back face
+                                7, 6, 4, 7, 4, 5,
                         }));
         gameItems.add(gameItem);
         gameItem.setPosition(0, 0, -2);
@@ -99,11 +128,11 @@ public class DummyGame implements IGameLogic {
             gameItem.setScale(scale);
 
             // Update rotation angle
-            float rotation = gameItem.getRotation().z + 1.5f;
+            float rotation = gameItem.getRotation().x + 1.5f;
             if (rotation > 360) {
                 rotation = 0;
             }
-            gameItem.setRotation(0, 0, rotation);
+            gameItem.setRotation(rotation, rotation, rotation);
         }
     }
 
