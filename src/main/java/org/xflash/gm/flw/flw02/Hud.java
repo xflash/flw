9b0e8d92188a9483766027f22b90fe62be6fafca
Rémy Coqueugniot <rcoqueugniot@gmail.com>
2017-05-17 06:56:58 +0200

    Introducing compass

--- src/main/java/org/xflash/gm/flw/flw02/Hud.java
+++ src/main/java/org/xflash/gm/flw/flw02/Hud.java
@@ -5,6 +5,9 @@ import org.xflash.engine.GameItem;
 import org.xflash.engine.IHud;
 import org.xflash.engine.TextItem;
 import org.xflash.engine.Window;
+import org.xflash.engine.graph.Material;
+import org.xflash.engine.graph.Mesh;
+import org.xflash.engine.graph.OBJLoader;
 
 public class Hud implements IHud {
 
@@ -17,15 +20,33 @@ public class Hud implements IHud {
     private final GameItem[] gameItems;
 
     private final TextItem statusTextItem;
+    private final GameItem compassItem;
+
 
     public Hud(String statusText) throws Exception {
         this.statusTextItem = new TextItem(statusText, FONT_TEXTURE, FONT_COLS, FONT_ROWS);
         this.statusTextItem.getMesh().getMaterial().setAmbientColour(new Vector4f(1, 1, 1, 1));
-        gameItems = new GameItem[]{statusTextItem};
+
+        // Create compass
+        Mesh mesh = OBJLoader.loadMesh("/models/compass.obj");
+        Material material = new Material();
+        material.setAmbientColour(new Vector4f(1, 0, 0, 1));
+        mesh.setMaterial(material);
+        compassItem = new GameItem(mesh);
+        compassItem.setScale(40.0f);
+        // Rotate to transform it to screen coordinates
+        compassItem.setRotation(0f, 0f, 180f);
+
+        // Create list that holds the items that compose the HUD
+        gameItems = new GameItem[]{statusTextItem, compassItem};
     }
 
     public void setStatusText(String statusText) {
         this.statusTextItem.setText(statusText);
+    }
+
+    public void rotateCompass(float angle) {
+        this.compassItem.setRotation(0, 0, 180 + angle);
     }
 
     @Override
@@ -35,5 +56,6 @@ public class Hud implements IHud {
 
     public void updateSize(Window window) {
         this.statusTextItem.setPosition(10f, window.getHeight() - 50f, 0);
+        this.compassItem.setPosition(window.getWidth() - 40f, 50f, 0);
     }
 }
